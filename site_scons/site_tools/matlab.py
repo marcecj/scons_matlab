9c86e9403f2fec12f1b2e3a701ea665db85eb490
mjoliet <mjoliet@51b913f5-ecc8-4856-93ce-2e549f94dee3>
2010-04-26 07:18:50 +0000

    Rework mex.py, rename back to matlab.py.
    Reworked mex.py and renamed it back to the more fitting matlab.py (because it
    doesn't work only for mex extensions; the original matlab.py is now
    matlab_old.py).  It now accepts a boolean "mex" option that, if set to a true
    value, will preset some environment variables for compiling mex extensions.
    Otherwise a few internal things were reworked, e.g, now the "with" statement is
    used for automatically closing the temporary file.

--- site_scons/site_tools/matlab.py
+++ site_scons/site_tools/matlab.py
@@ -1,55 +1,66 @@
 # matlab.py:  Matlab extension builder
 # 
 # by Marc Joliet, marcec@gmx.de, 2010.04.22
-# (based on mex.py by Joe VanAndel, vanandel@ucar.edu, 2010/1/15)
+# (loosely based on mex.py by Joe VanAndel, vanandel@ucar.edu, 2010/1/15)
 #
 # This is a little SCons builder tool that finds Matlabs installation directory
 # and sets various variables of the build environment. To do that, it starts a
-# Matlab subprocess, issues a few commands, and parses the result.
+# Matlab subprocess that issues a few commands that print to a file, and parses
+# the file.
 #
-# NOTE: This only works on Unix, as Matlab under Windows doesn't work with pipes
-#
-# TODO: find out if results can be cached
+# TODO: find out if results can be cached. as a workaround, use interactive mode
+# and use the "build" command (short form "b").
 
-import os, re
-from subprocess import Popen, PIPE
+import os, tempfile, subprocess as subp, sys
 
-def generate(env):
-    # invoke matlab
-    proc = Popen(['matlab', '-nosplash'], stdin=PIPE, stdout=PIPE)
+def generate(env, **kwargs):
+    # determine if the env is supposed to be a mex extension, default to False
+    is_mex_ext = kwargs.get('mex', False)
 
-    # get mex extension, matlab installation root and Matlab arch
-    stdout, stderr = proc.communicate('\n'.join( ['mexext', 'matlabroot',
-        "getenv('MATLAB_ARCH')", 'quit']))
+    # invoke matlab, method of doing so taken from the mlabwrap setup.py
+    with tempfile.NamedTemporaryFile() as tmp_file:
+        # The usage of '10' as a newline char is needed because... maybe Python
+        # universal newlines translate to newlines Matlab doesn't like? I dunno, but
+        # in the Matlab command line '\n' works, but not in this script, even with
+        # escapes or as a raw string.
+        matlab_cmd = "fid = fopen('%s', 'wt');" % tmp_file.name + \
+                r"fprintf(fid, '%s%c%s%c%s%c', mexext, 10, matlabroot, 10, computer, 10);" + \
+                "fclose(fid);quit;"
+        if os.name == "nt":
+            matlab_cmd = '"' + matlab_cmd + '"'
+        cmd_line = ['matlab', '-nodesktop', '-nosplash', '-r', matlab_cmd]
 
-    # get lines not starting with space or newline, and empty lines
-    pat   = re.compile("^(\s+.*)*$")
-    lines = [l for l in stdout.split('\n') if not pat.findall(l)]
+        try:
+            # output to pipe to suppress output on Unix
+            subp.check_call(cmd_line, stdout=subp.PIPE)
+        except (OSError, subp.CalledProcessError) as e:
+            # PEP 352 can't go ahead quick enough, stupid args tupel. I want the
+            # message attribute back!
+            print >> sys.stderr, "Error:", ', '.join([repr(i) for i in e.args])
+            exit("Error calling Matlab!")
 
-    # return value is every 3 lines: prompt, 'ans =', and then the actual answer
-    matlab_root = lines[4].rstrip('\n')
-    matlab_arch = lines[7].rstrip('\n')
+        # read lines from file and remove newline chars
+        lines = [l.strip('\n') for l in tmp_file.file.readlines()]
+
+    matlab_root = lines[1]
+    matlab_arch = lines[2].lower()
     env['MATLAB'] = {
-            "MEX_EXT":  "." + lines[1].rstrip('\n'),
+            "MEX_EXT":  "." + lines[0],
             "ROOT":     matlab_root,
             "ARCH":     matlab_arch,
             "SRC":      ''.join([matlab_root, os.sep, 'extern', os.sep, 'src']),
             "INCLUDE":  ''.join([matlab_root, os.sep, 'extern', os.sep, 'include']),
             "LIB_DIR":  ''.join([matlab_root, os.sep, 'bin', os.sep, matlab_arch])
             }
+    env.Append(CPPPATH = [env['MATLAB']['INCLUDE']],
+            LIBPATH = [env['MATLAB']['LIB_DIR']])
 
-    # TODO: maybe add option for the special case of Mex files instead of
-    # assigning these Vars by default
-    env.Replace(SHLIBPREFIX="", SHLIBSUFFIX=env['MATLAB']['MEX_EXT'])
-    env.Append(
-            CPPPATH     = [env['MATLAB']['INCLUDE']],
-            LIBPATH     = [env['MATLAB']['LIB_DIR']],
-            CPPDEFINES  = ["MATLAB_MEX_FILE"]
-            )
+    if is_mex_ext:
+        env.Replace(SHLIBPREFIX="", SHLIBSUFFIX=env['MATLAB']['MEX_EXT'])
+        env.Append(CPPDEFINES=["MATLAB_MEX_FILE"])
 
 def exists(env):
-    # TODO: maybe add more tests here, don't know if we need them
-    # TODO: add warning when Matlab doesn't exist
-    if env.WhereIs("matlab") is None:
+    # FIXME: why is this function not called? Calling exit() here does nothing!
+    if not env.WhereIs("matlab"):
         return False
     return True
